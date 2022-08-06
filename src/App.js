da3253510509d123cec8d4f8b82c21e324f7d37d
Yunus Özkaya <nozkaya.mdbf20@iste.edu.tr>
2022-08-06 05:48:07 +0300

    new desing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+import data from './components/data';
 
 function App() {
+  const newCard= data.map(card=>{
+    return <Card key={card.id}
+    img={card.coverImg} rating={card.stats.rating} reviewCount={card.stats.reviewCount} country={card.location} title={card.title} price={card.price} openSpots={card.openSpots} /> })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar />
+      <Hero />
+      <section className='cards-list'>
+      {newCard}
+      </section>
     </div>
   );
 }
 
 export default App;
+  
