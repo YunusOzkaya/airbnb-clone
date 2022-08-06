bdca6a51ae0d21c4937b65139f5cef572b44aec5
Yunus Özkaya <nozkaya.mdbf20@iste.edu.tr>
2022-08-06 05:07:09 +0300

    güncelleme

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Hero(){
+    return(
+        <div className="hero">
+            <img src="images/group77.png" />
+            <h2>Online Experiences</h2>
+            <div id="hero-p">
+            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+    <div className="navbar">
+        <img src="images/airbnb1.png" />
+    </div>)
+}
